f0cfe16f78326d86ab61eb409f0da00646b5ec7f
mvskiran <marisetty.saikiran@gmail.com>
2017-04-12 13:52:26 -0500

    adde some modifications

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'validates_email_format_of'
 
 group :development, :test do
   gem 'sqlite3'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-
-end
+  has_many :articles
+  before_save{self.email = email.downcase}
+   validates :username, presence: true, 
+   length: {minimum: 3, maximum: 25}, 
+   uniqueness: {case_sensitive: false}
+    validates :email, presence: true,
+   length: {maximum: 50}, uniqueness: {case_sensitive: false},
+   :email_format => { :message => 'is not looking good' }
+ end
