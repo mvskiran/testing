1793819a3c118841e4b8a10cf6d08b6164f0907e
mvskiran <marisetty.saikiran@gmail.com>
2017-05-02 09:22:47 -0500

    added debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,9 @@ class ArticlesController < ApplicationController
 	def edit
 	end
 	def create
+	 debugger
 	 @article = Article.new(article_params)
+	 @article.user = User.first 
 	 if @article.save
 	 flash[:success] = "article was successfully created"
 	 redirect_to article_path(@article)
